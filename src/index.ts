0eed490c968761c5d446b11f0b14f0e8446ed59c
Sandon Lai <sandonnlai@gmail.com>
2021-12-21 20:31:00 +1100

    v1.2 - added GraphQL schema read post and createPost

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post";
+import 'reflect-metadata';
 import microConfig from "./mikro-orm.config"
 import express from 'express'
 import {ApolloServer } from 'apollo-server-express'
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 
 const main = async () => {
@@ -21,9 +23,10 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false
-        })
+        }),
+        context: () => ({ em: orm.em })
     })
 
     apolloServer.applyMiddleware({ app })
