9c7d0ae4579075bc89b0af0dff0740e320560d7f
Sandon Lai <sandonnlai@gmail.com>
2022-03-07 23:01:10 +1100

    v1.4.1 - minor changes to user resolver to include validation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,24 +1,128 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
-import  argon2 from "argon2";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
+import argon2 from "argon2";
+
+// Input types we can use for arguments
 @InputType()
 class UsernamePasswordInput {
-    @Field()
-    username: string
-    @Field()
-    password: string
+  @Field()
+  username: string;
+  @Field()
+  password: string;
 }
+
+// Input validation for field entries
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+// We can return object types from our mutations
+// Question Mark - user return if worked properly otherwise an error is returned (optional)
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
-    async register(
-        @Arg('options') options: UsernamePasswordInput,
-        @Ctx() {em}: MyContext
-    ) {
-        const hashedPassword = await argon2.hash(options.password); 
-        const user = em.create(User, {username: options.username, password: hashedPassword})
-        await em.persistAndFlush(user); 
-        return user;
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username length must be greater than 2",
+          },
+        ],
+      };
     }
+    if (options.password.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Password length must be greater than 3",
+          },
+        ],
+      };
+    }
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      console.log("message :", err.message);
+      if (err.code === "23505") {
+        // || err.detail.includes("already exists")) {
+        // duplicate username error
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "username is already taken",
+            },
+          ],
+        };
+      }
+    }
+
+    return {
+      user,
+    };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "That username doesn't exist.",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "That password is incorrect.",
+          },
+        ],
+      };
+    }
+    return {
+      user,
+    };
+  }
 }
