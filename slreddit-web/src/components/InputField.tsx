f8acc7e13318804139e04e150f024dbda65afca1
Sandon Lai <sandonnlai@gmail.com>
2022-03-13 17:26:56 +1100

    v2.1.1 - added urql and altered input components

---
+++ slreddit-web/src/components/InputField.tsx
@@ -0,0 +1,31 @@
+import {
+  FormLabel,
+  FormErrorMessage,
+  FormControl,
+} from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { useField } from "formik";
+import React, { InputHTMLAttributes } from "react";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  name: string;
+};
+
+// '' => false
+// 'error message' => true
+// Generic input field
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _, // Destructure size out of props with _
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
--- slreddit-web/src/pages/register.tsx
+++ slreddit-web/src/pages/register.tsx
@@ -1,42 +1,69 @@
 import React from "react";
 import { Form, Formik } from "formik";
-import {
-	FormControl,
-	FormLabel,
-	Input,
-	FormErrorMessage,
-} from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { Box } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { useMutation } from "urql";
 
-interface registerProps { }
+interface registerProps {}
 
-const Register: React.FC<registerProps> = ({ }) => {
-	// test comment
-	return (
-		<Wrapper variant="small">
-			<Formik
-				initialValues={{ username: "", password: "" }}
-				onSubmit={(values) => {
-					console.log(values);
-				}}
-			>
-				{({ values, handleChange }) => (
-					<Form>
-						<FormControl>
-							<FormLabel htmlFor="username">Username</FormLabel>
-							<Input
-								value={values.username}
-								onChange={handleChange}
-								id="username"
-								placeholder="username"
-							/>
-							{/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-						</FormControl>
-					</Form>
-				)}
-			</Formik>
-		</Wrapper>
-	)
+// Register GraphQL Query (Mutation)
+const REGISTER_MUT = `
+mutation Register($username:String!, $password:String! ) {
+  register(options: { username: $username, password: $password }) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+`;
+
+const Register: React.FC<registerProps> = ({}) => {
+  // Second element is our function
+  const [, register] = useMutation(REGISTER_MUT);
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          return register(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="Password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={5}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+              size="sm"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
 export default Register;
