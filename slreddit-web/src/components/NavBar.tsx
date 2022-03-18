e3035a6fd961e945701ed234b4de229c0a67f16f
Sandon Lai <sandonnlai@gmail.com>
2022-03-18 18:47:06 +1100

    v2.1.4 - updated navbar states for login, register and unregistered

--- slreddit-web/src/components/NavBar.tsx
+++ slreddit-web/src/components/NavBar.tsx
@@ -1,20 +1,41 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml={"auto"}>
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  // Data is loading
+  if (fetching) {
+    // User not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href="/login">
           <Link mr={2}>Login</Link>
         </NextLink>
         <NextLink href="/register">
           <Link mr={2}>Register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+
+    // User is logged in
+  } else {
+    body = (
+      <Flex>
+        <Box mr={4}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
 };
